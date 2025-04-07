b37bfc5b7a6f8abe0582c16ae77dbd4084182431
Samruddhi Patil <samruddhi.patil@rockets.utoledo.edu>
2025-04-07 17:14:01 -0400

    First commit

---
+++ SE_project_GetToKnowYourself.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SE_project_GetToKnowYourself.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
